b7c28528d4d56e132b9ce954199415451546cf56
Michael Tran <solidwater969@gmail.com>
2022-02-15 22:53:45 -0500

    refactor: remove files

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,21 +1,13 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
-import { BubbleComponent } from './components/bubble/bubble.component';
-import { ButtonComponent } from './components/button/button.component';
-import { GraveyardComponent } from './components/graveyard/graveyard.component';
-import { ImageComponent } from './components/image/image.component';
-import { VirtualPetComponent } from './components/virtual-pet/virtual-pet.component';
+import { GraveyardComponent } from './graveyard.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    BubbleComponent,
-    ButtonComponent,
-    GraveyardComponent,
-    ImageComponent,
-    VirtualPetComponent
+    GraveyardComponent
   ],
   imports: [BrowserModule],
   bootstrap: [AppComponent]
